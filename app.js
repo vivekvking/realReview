5cf01b96c391f64d0bc29536a594edfdfae4b986
Vivek Kumar <vivek.kakadan@oriserve.com>
2024-02-26 18:16:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.use("/", (req, res) => {
+  res.send("Heyyy Server Started")
+});
+
+app.listen(process.env.PORT, ()=> {
+  console.log("Server started on port ", process.env.PORT);
+})
