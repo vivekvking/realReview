9ca31e9da9ce1176c5dcbe20c12d9bfb1dbd0c50
Vivek Kumar <vivek.kakadan@gmail.com>
2024-03-01 00:49:37 +0530

    added files in format for review and user

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 app.use("/", (req, res) => {
-  res.send("Heyyy Server Started")
+    res.send("Heyyy Server Started");
 });
 
-app.listen(process.env.PORT, ()=> {
-  console.log("Server started on port ", process.env.PORT);
-})
+app.listen(process.env.PORT, () => {
+    console.log("Server started on port ", process.env.PORT);
+});
